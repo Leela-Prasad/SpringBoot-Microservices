523f5374401372a194385b3e1d611e421e147be3
Leela <leela.jagu@gmail.com>
2019-02-20 09:46:09 +0530

    Initial Setup for Microservices - Active MQ, PositionSimulator, Positiontracking,Fleetman

---
+++ positiontracking/src/main/java/com/virtualpairprogrammers/PositionQueueListener.java
@@ -0,0 +1,21 @@
+package com.virtualpairprogrammers;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PositionQueueListener {
+
+	@Autowired
+	private Data data;
+	
+	
+	@JmsListener(destination="positionQueue	")
+	public void process(Map<String,String> message) {
+		data.updatePosition(message);
+		System.out.println("consuming message ..." + message);
+	}
+}
