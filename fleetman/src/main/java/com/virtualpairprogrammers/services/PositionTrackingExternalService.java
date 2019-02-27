36c90b4e2fa589b9ffc7307587c215a5fb4e915a
Leela-Prasad <leela.jagu@gmail.com>
2019-02-27 07:13:41 +0530

    Update PositionTrackingExternalService.java
    Removed Semaphore mode as JPA code is moved to controller.

--- fleetman/src/main/java/com/virtualpairprogrammers/services/PositionTrackingExternalService.java
+++ fleetman/src/main/java/com/virtualpairprogrammers/services/PositionTrackingExternalService.java
@@ -1,8 +1,5 @@
 package com.virtualpairprogrammers.services;
 
-import java.math.BigDecimal;
-import java.util.Date;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
@@ -10,7 +7,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.virtualpairprogrammers.controllers.Position;
 import com.virtualpairprogrammers.data.VehicleRepository;
 import com.virtualpairprogrammers.domain.Vehicle;
@@ -27,8 +23,7 @@ public class PositionTrackingExternalService {
 	//In fallback mechanism we will wrap the code which may throw an exception into a separate method
 	//and annotate that method with @HystrixCommand which indicates the fallback that needs to be 
 	//run in case of failures.
-	@HystrixCommand(fallbackMethod="handleExternalServiceDown",
-					commandProperties= {@HystrixProperty(name="execution.isolation.strategy", value="SEMAPHORE")})
+	@HystrixCommand(fallbackMethod="handleExternalServiceDown")
 	public Position getLastestPositionForVehicleFromRemoteMicroservice(String name) {
 		RestTemplate rest = new RestTemplate();
 		
